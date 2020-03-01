5ad1cf5f1cf43eff90c420917eb5584e7800e2e4
Alex Muhich <feldtlaufer@gmail.com>
2020-02-29 18:09:54 -0600

    server.js updated

--- server.js
+++ server.js
@@ -7,9 +7,25 @@ const app = express();
 //connect database
 connectDatabase();
 
+//configure middleware
+app.use(express.json({ extended: false }));
+
 //api endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
     res.send("http get request sent to root api endpoint"));
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 //connection listener
 app.listen(3000, () => console.log("Express server running on port 3000"));
