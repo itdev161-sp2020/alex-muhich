d504a71d787a42606b942378bc2cd137d4e84df9
Alex Muhich <feldtlaufer@gmail.com>
2020-03-25 14:16:20 -0500

    added token

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
 import User from './models/User';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 //initialize express application
 const app = express();
@@ -60,7 +62,19 @@ app.post('/api/users',
 
                 //save to the db and return
                 await user.save();
-                res.send('User successfully registerd');
+                //generate and return a jwt token
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+                jwt.sign(payload, 
+                    config.get('jwtsecret'),
+                    { expiresIn: '10hr' },
+                    (err, token) => {
+                        if(err) throw err;
+                        res.json({ token: token });
+                    });
 
             }catch(error){
                 res.status(500).send('Server error');
